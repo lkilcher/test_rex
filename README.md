5246b0416fa76a595ecf65fb8d39795740ee2f00
Levi Kilcher <levi.kilcher@gmail.com>
2021-03-11 13:34:41 -0700

    Update the script.

---
+++ README.md
@@ -0,0 +1,11 @@
+Start by setting up a conda environment (this doesn't work on the NREL network):
+
+    conda env create -f environment.yml
+    
+And activate it:
+
+    conda activate rex-test
+    
+You'll need to [register for an NREL API key](https://developer.nrel.gov/signup/), and configure hsds (as described [here](https://github.com/openEDI/documentation/blob/master/US_Wave.md)).
+
+Now you should be able to run the script.
--- scatter-jpd.py
+++ scatter-jpd.py
@@ -1,17 +1,45 @@
 from rex import MultiYearWaveX
 import pandas as pd
+from hist_scatter import scat_hist2d
+import matplotlib.pyplot as plt
+plt.ion()
+import numpy as np
+
+datname = f'west_coast_gg50.csv'
+
+try:
+    dat = pd.read_csv(datname, index_col=0)
+
+except FileNotFoundError:
+
+    lat_lon = (37.541, -123.365) # ~50 miles offshore of the Golden Gate
+    outname = 'west_coast_gg50.csv'
+    
+    wavepath = '/nrel/US_wave/West_Coast/'
+    years = list(range(1979, 2011)) # All available years
+
+    parameters = ['significant_wave_height', 'peak_period', 'energy_period']
+    tmp = []
+    with MultiYearWaveX(wavepath, years=years, hsds=True) as mYears:
+        for p in parameters:
+            tmp.append(mYears.get_lat_lon_df(p, lat_lon))
+
+    dat = pd.DataFrame({p: d.values[:, 0] for p, d in zip(parameters, tmp)}, index=tmp[0].index)
+
+    dat.to_csv(outname)
 
 
-lat_lon = (37.541, -123.365) # ~50 miles offshore of the Golden Gate
+fignum = 10
+fig = plt.figure(fignum)
+fig.clf()
+fig, ax = plt.subplots(1, 1, num=fignum)
 
-wavepath = '/nrel/US_wave/West_Coast/' # All years
-years = list(range(1980, 2011))
+h = scat_hist2d(dat['energy_period'],dat['significant_wave_height'], bins=[np.arange(5, 20, 0.25), np.arange(0, 12, 0.25)], dens_func=None, normed=True)
+cbar = plt.colorbar(h)
+cbar.set_label('1/(m sec)')
+ax.set_xlabel('Energy Period [sec]')
+ax.set_ylabel('Significant Wave Height [m]')
+ax.set_title('Wave Energy Joint Probability Distribution\n50 miles offshore of the Golden Gate')
 
-parameters = ['significant_wave_height', 'peak_period', 'energy_period']
-tmp = []
-with MultiYearWaveX(wavepath, years=years, hsds=True) as mYears:
-    for p in parameters:
-        tmp.append(mYears.get_lat_lon_df(p, lat_lon))
-
-df = pd.DataFrame({p: d.values[:, 0] for p, d in zip(parameters, tmp)}, index=tmp[0].index)
+fig.savefig('JPD_GoldenGate.png', dpi=300)
 
